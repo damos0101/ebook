e62d27604d74d8bc07c3706ca6015bd1a4c12251
damos0101 <moskvindnl01@gmail.com>
2023-12-18 15:25:46 +0200

    Commit

--- ebook/src/main/java/com/ebooks/ebook/exception/BookNotFoundException.java
+++ ebook/src/main/java/com/ebooks/ebook/exception/BookNotFoundException.java
@@ -2,6 +2,6 @@ package com.ebooks.ebook.exception;
 
 public class BookNotFoundException extends RuntimeException {
     public BookNotFoundException(int id) {
-        super("Не знайдено користувача з id" + id);
+        super("Не знайдено книжку з id" + id);
     }
 }
